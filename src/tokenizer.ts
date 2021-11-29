f5751a44bab58fd5b7c89e1ffa780c284293ffe2
Nanda Syahrasyad <nanda.s@hey.com>
2021-11-28 20:44:29 -0800

    feat: implement simple tokenizer

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -1,3 +1,52 @@
 export function tokenize(input: string): string[] {
-  return input.split(" ");
+  let start = 0;
+  let current = 0;
+  const tokens = [];
+
+  function finishIdentifier() {
+    while (isAlpha(input[current + 1])) {
+      current++;
+    }
+    current++;
+    const token = input.substring(start, current);
+    console.log(token);
+    start = current;
+    return token;
+  }
+
+  while (current < input.length) {
+    const currentChar = input[current];
+
+    if (isWhitespace(currentChar)) {
+      start++;
+      current++;
+      continue;
+    }
+
+    if (isAlpha(currentChar)) {
+      tokens.push(finishIdentifier());
+    } else if (isSingleCharacter(currentChar)) {
+      tokens.push(currentChar);
+      start++;
+      current++;
+    } else {
+      throw new Error(`Unknown character: ${currentChar}`);
+    }
+  }
+
+  return tokens;
+}
+
+function isAlpha(char: string) {
+  return !isWhitespace(char) && /[a-zA-Z]/.test(char);
+}
+
+function isWhitespace(char: string) {
+  return /\s/.test(char);
+}
+
+const knownSingleCharacters = new Set(["(", ")", "{", "}", ".", ";"]);
+
+function isSingleCharacter(char: string) {
+  return knownSingleCharacters.has(char);
 }
---
+++ tokenizer.md
@@ -0,0 +1,4 @@
+# Tokenizer
+
+1. If it starts with a letter, it's an identifier
+2. There's also the simple one character tokens: `(`, `)`, `{`, `}`, `.` and `;`
